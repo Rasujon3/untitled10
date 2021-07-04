f34e6a7e13dff662ed89346d4ab723e295c854c1
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-07-04 18:08:10 +0600

    hello world in func component
    hello world in func component

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
-import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import React from 'react';
+import { Text, View } from 'react-native';
 
-class App extends Component {
-    render() {
-        return (
-            <View>
-                <Text>React Native Class</Text>
-            </View>
-        );
-    }
+const HelloWorldApp = () => {
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center"
+            }}>
+            <Text>Hello, world!</Text>
+        </View>
+    )
 }
-
-export default Cafe;
+export default HelloWorldApp;
