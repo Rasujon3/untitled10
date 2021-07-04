0c6aaadb0164a45e810fbe14411aa688a020d43b
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-07-04 20:34:07 +0600

    Update App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+
+class App extends Component {
+    render() {
+        return (
+            <View style={{alignSelf:"auto",alignContent:"center",alignItems:"center",textAlign:"center"}}>
+                <Text style={{alignSelf:"center",alignContent:"center",textAlign:"center"}}>Class Test</Text>
+            </View>
+        );
+    }
+}
+
+export default App;
