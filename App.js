c9afd1f397582df6c4a3ba1f26c5315a32803de6
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-07-04 18:14:55 +0600

    hello world in class component
    hello world in class component

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
 
-const HelloWorldApp = () => {
-    return (
-        <View
-            style={{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center"
-            }}>
-            <Text>Hello, world!</Text>
-        </View>
-    )
+class App extends Component {
+    render() {
+        return (
+            <View
+                style={{
+                    flex: 1,
+                    justifyContent: "center",
+                    alignItems: "center"
+                }}>
+                <Text>Hello, world!</Text>
+            </View>
+        );
+    }
 }
-export default HelloWorldApp;
+
+export default App;
